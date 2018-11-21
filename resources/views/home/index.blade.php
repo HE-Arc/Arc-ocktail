f3c7fab20513f0c9c5e0ba4e0be5679fd67df06d
noahgraells <graells.noah@gmail.com>
2018-11-21 09:40:44 +0100

    Fixing css

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,54 +6,57 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-md-9">
-        <div class="row">
-            <ul class="nav nav-tabs col-md-12">
-                @foreach ($data['categories'] as $categorie)
-                    <button id="{{$categorie->name}}Button">{{$categorie->name}}</button>
-                @endforeach
+    <div class="row m-2">
+        <div class="col-sm-12 col-md-8 col-lg-8">
+            <div class="row">
+                <div class="btn-group p-2" role="group" aria-label="Basic example">
+                    @foreach ($data['categories'] as $categorie)
+                        <button id="{{$categorie->name}}Button" type="button" class="btn btn-light">{{$categorie->name}}</button>
+                    @endforeach
+                </div>
+            </div>
+            <div id="ingredients" class="row"></div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4">
+            <h2>Vos ingrédients</h2>
+            <ul class="list-group">
+                <!-- TODO - foreach selected ingredients -->
+                <!-- <li class="list-group-item">Citron</li> -->
             </ul>
-
-            <div id="container" class="container"></div>
         </div>
     </div>
 
-    <div class="col-md-3">Ingerdients</div>
-</div>
 @endsection
 
 @section('script')
 
-  @foreach ($data['categories'] as $key => $categorie)
-    <script type="text/javascript">
-      $('#{{$categorie->name}}Button').on('click', function()
-      {
-        $.get("{{ URL::to('read-data') }}", function(data)
-        {
-          $('#container').html("");
-          $.each(data.ingredients[{{$key}}], function(i, value)
-          {
-            var tr = $([
-              "<div class='col-sm-12 col-md-6 col-lg-4'>",
-              "  <div class='card'>",
-              "    <img class='card-img-top' src='", value.name, ".png'>",
-              "    <div class='card-body'>",
-              "      <h5 class='card-title'>",
-                       value.name,
-              "      </h5>",
-              "      <a href='#' class='btn btn-primary'>Ajouter</a>",
-              "    </div>",
-              "  </div>",
-              "</div>"
-            ].join("\n"));
-            $('#container').append(tr);
-          })
-
-        })
-
-      });
-    </script>
-  @endforeach
+    @foreach ($data['categories'] as $key => $categorie)
+        <script type="text/javascript">
+            $('#{{$categorie->name}}Button').on('click', function()
+            {
+                $.get("{{ URL::to('read-data') }}", function(data)
+                {
+                    $('#ingredients').html("");
+                    $.each(data.ingredients[{{$key}}], function(i, value)
+                    {
+                        var tr = $([
+                        "<div class='col-sm-12 col-md-6 col-lg-4 p-2'>",
+                        "  <div class='card'>",
+                        "    <img class='card-img-top p-1' src='uploads/", value.name, ".jpg'>",
+                        "    <div class='card-body'>",
+                        "      <h5 class='card-title'>",
+                            value.name,
+                        "      </h5>",
+                        "      <a href='#' class='btn btn-primary'>Ajouter</a>",
+                        "    </div>",
+                        "  </div>",
+                        "</div>"
+                        ].join("\n"));
+                        $('#ingredients').append(tr);
+                    })
+                })
+            });
+        </script>
+    @endforeach
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        <link href="{{ asset('css/custom.css') }}" rel="stylesheet" type="text/css" >
+        <!-- <link href="{{ asset('css/custom.css') }}" rel="stylesheet" type="text/css" >-->
 
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
@@ -14,29 +14,32 @@
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="{{ asset('icons/logo.png') }}" width="30" height="30" class="d-inline-block align-top" alt="">
-                Arc'ocktail
-            </a>
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('icons/logo.png') }}" width="30" height="30" class="d-inline-block align-top" alt="">
+                    Arc'ocktail
+                </a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
 
                 <form class="form-inline my-2 my-lg-0">
                     <button class="btn btn-outline-primary mr-2 my-2 my-sm-0" type="submit">
                         <span class="glyphicon glyphicon-tint" aria-hidden="true"></span>Login
-                        </button>
+                    </button>
                     <button class="btn btn-primary my-2 my-sm-0" type="submit">Register</button>
                 </form>
             </div>
-        </div>
         </nav>
-        @yield('content')
-        @yield('script')
+
+        <div class="container border rounded p-0">
+            @yield('content')
+            @yield('script')
+        </div>
+
         <footer class="row">
-		          <div class="col-md-12">Pied de page</div>
-		</footer>
+            <div class="col-md-12">Pied de page</div>
+        </footer>
     </body>
 </html>
