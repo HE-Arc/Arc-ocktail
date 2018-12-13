0b28dc237c14866aaad556be60e1354a39bd7435
Hüsler Etienne <etienne.husler@he-arc.ch>
2018-12-13 16:35:29 +0100

    URL of images for the deployment

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -98,7 +98,7 @@
                       var tr = $([
                       "<div id='" + (value.name + "Card").replace(/\s+/g, '') + "' class='col-6 col-md-6 col-lg-4 p-2'>",
                       "  <div class='card rounded-0 border-0'>",
-                      "    <img class='card-img-top p-1' src='uploads/", value.name, ".jpg'>",
+                      "    <img class='card-img-top p-1' src='" + '{{url('/uploads/')}}' + "/" + value.name + "'.jpg'>",
                       "    <div class='card-body p-3'>",
                       "      <h5 class='card-title text-dark'>",
                           value.name,
